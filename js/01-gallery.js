4152ebb5f9092e1f56a6ad648738db6364ddb052
StanislavPavlenko <voltfrag@gmail.com>
2023-05-26 23:48:29 +0300

    12312

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-const galleryItemsEl = document.querySelector('.gallery');
 
 
-galleryItemsEl.addEventListener('click', onClick)
+const gallery = document.querySelector('.gallery');
+
+
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+         <a class="gallery__link" href="${original}">
+           <img
+             class="gallery__image"
+             src="${preview}"
+             data-source="${original}"
+             alt="${description}"
+           />
+         </a>
+       </li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+gallery.addEventListener('click', onClick);
+
+const instance = basicLightbox.create(`
+    <img src="">
+`)
+
+const imgEl = instance.element().querySelector('img');
+
+function onClick(e) {
+    if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+    e.preventDefault();
+    const imgBig = e.target.dataset.source;
+    imgEl.src = imgBig;
+    instance.show()
+    
+}
+
